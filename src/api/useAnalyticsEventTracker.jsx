d51b5eb712b2d6f3499336f4c587cb07598edfe7
engendromestre <engendro.mestre@gmail.com>
2022-11-14 21:50:46 -0300

    modified Nav Component add api google analytics 14

--- src/api/useAnalyticsEventTracker.jsx
+++ src/api/useAnalyticsEventTracker.jsx
@@ -1,6 +1,9 @@
 import ReactGA from "react-ga";
 
-const useAnalyticsEventTracker = (category,action) => {
-  ReactGA.event({category, action});
+const useAnalyticsEventTracker = (category,_action) => {
+  const eventTracker = (_action = "test action", _label = "test label") => {
+    ReactGA.event({category, _action, _label});
+  }
+  return eventTracker;
 }
 export default useAnalyticsEventTracker;
