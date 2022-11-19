bce142bd21b0a644c47b6b27e2befa34e69ee871
engendromestre <engendro.mestre@gmail.com>
2022-11-19 17:31:45 -0300

    config package googleTagManager

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from "react-dom/client"
+import TagManager from "react-gtm-module";
 import App from "./App"
 import "./index.css"
+
+const tagManagerArgs = {
+    gtmId: 'GTM-KK464QL'
+  }
+  TagManager.initialize(tagManagerArgs)
 
 const root = ReactDOM.createRoot(document.querySelector("#root"))
 root.render(<App />)
